0823930ef0cc8869a3f4ccd698ad6bf65631a8a2
xwartz <stddup@gmail.com>
2017-04-13 21:53:16 +0800

    Fix logout

--- src/signout.js
+++ src/signout.js
@@ -3,7 +3,7 @@ var signoutUrl = 'https://github.com/'
 module.exports = {
   start: function (casper) {
     casper.thenOpenAndEvaluate(signoutUrl, function () {
-      var form = document.forms[1]
+      var form = document.querySelectorAll('form[action*="/logout"]')[0]
       form.submit()
     })
   }
